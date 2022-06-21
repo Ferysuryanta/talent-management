d60fed30cb4cb479eceab4277d33857e184f5071
Dwiki Kurniawan <Dwiki_K782@mitrais.com>
2022-06-21 11:36:58 +0700

    change entity relational of talent and comment

--- src/main/java/com/mitrais/mschassis/entity/Comment.java
+++ src/main/java/com/mitrais/mschassis/entity/Comment.java
@@ -2,9 +2,17 @@ package com.mitrais.mschassis.entity;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Data;
 
@@ -13,13 +21,18 @@ import lombok.Data;
 @Table(name = "mst_comment")
 public class Comment {
 
-  @Id
-  private String id;
-  private String talentName;
-  private Timestamp timestamp;
-  private String createdBy;
-  private String category;
-  private String description;
-  private String status;
-
+	@Id
+	@GeneratedValue(generator = "comment_uuid")
+	@GenericGenerator(name = "comment_uuid", strategy = "uuid")
+	@Column(unique = true)
+	private String id;
+	@ManyToOne
+    @JoinColumn
+    @JsonIgnoreProperties(value ={"comments"})
+	private Talent talent;
+	private Timestamp timestamp;
+	private String createdBy;
+	private String category;
+	private String description;
+	private String status;
 }
